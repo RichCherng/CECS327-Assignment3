5a3f803a9800bf5b56b0ad484d8ca75f01aaff1b
Rich Cherngchaosil <richer_po@hotmail.com>
2016-11-01 13:04:16 -0700

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args){
+
+		String[] ARRAY = new String[100];
+
+		String[] POOL = generatePool();
+	}
+
+	public static String[] generatePool(){
+
+		String[] arr = new String[125];
+
+		return null;
+	}
+}
